2070b81eed9dcb5e96a3b11eac5590078319d7f9
Natalie Cardot <33387780+nataliecardot@users.noreply.github.com>
2018-03-24 23:26:59 -0700

    Add color-fill functionality

--- designs.js
+++ designs.js
@@ -1,22 +1,22 @@
 const sizePicker = document.querySelector('.size-picker');
-const submitButton = document.querySelector('.submit-button');
 const pixelCanvas = document.querySelector('.pixel-canvas');
+const quickFill = document.querySelector('.quick-fill');
 
 function makeGrid() {
   let gridHeight = document.querySelector('.input-height').value;
   let gridWidth = document.querySelector('.input-width').value;
-  // if grid already present, clears any cells that have been filled in
+  // If grid already present, clears any cells that have been filled in
   while (pixelCanvas.firstChild) {
     pixelCanvas.removeChild(pixelCanvas.firstChild);
     }
-  // creates rows and cells
+  // Creates rows and cells
   for (let i = 1; i <= gridHeight; i++) {
     let gridRow = document.createElement('tr');
     pixelCanvas.appendChild(gridRow);
     for (let j = 1; j <= gridWidth; j++) {
       let gridCell = document.createElement('td');
       gridRow.appendChild(gridCell);
-      // fills in cell with selected color upon mouse press (unlike 'click', doesn't also require release of mouse button)
+      // Fills in cell with selected color upon mouse press ('mousedown', unlike 'click', doesn't also require release of mouse button)
       gridCell.addEventListener('mousedown', function() {
         // 'color' defined here rather than globally so JS checks whether user has changed color with each new mouse press on cell
         const color = document.querySelector('.color-picker').value;
@@ -26,22 +26,22 @@ function makeGrid() {
   }
 }
 
-// upon user's submitting height and width selections, callback function (inside method) calls makeGrid function. But event method preventDefault() first intercepts the 'submit' event, which would normally submit the form and refresh the page, preventing makeGrid() from being processed
+// Upon user's submitting height and width selections, callback function (inside method) calls makeGrid function. But event method preventDefault() first intercepts the 'submit' event, which would normally submit the form and refresh the page, preventing makeGrid() from being processed
 sizePicker.addEventListener('submit', function(e) {
   e.preventDefault();
   makeGrid();
 });
 
-// enables color dragging with selected color (code for filling in single cell is above)
+// Enables color dragging with selected color (code for filling in single cell is above)
 let down = false; // tracks whether or not mouse pointer is pressed
 
-// listens for mouse pointer press and release on grid. Changes value to true when pressed, but sets it back to false as soon as released
+// Listens for mouse pointer press and release on grid. Changes value to true when pressed, but sets it back to false as soon as released
 pixelCanvas.addEventListener('mousedown', function(e) {
 	down = true;
 	pixelCanvas.addEventListener('mouseup', function() {
 		down = false;
 	});
-  // ensures cells won't be colored if grid is left while pointer is held down
+  // Ensures cells won't be colored if grid is left while pointer is held down
   pixelCanvas.addEventListener('mouseleave', function() {
     down = false;
   });
@@ -49,7 +49,7 @@ pixelCanvas.addEventListener('mousedown', function(e) {
   pixelCanvas.addEventListener('mouseover', function(e) {
     // 'color' defined here rather than globally so JS checks whether user has changed color with each new mouse press on cell
     const color = document.querySelector('.color-picker').value;
-    // while mouse pointer is pressed and within grid boundaries, fills cell with selected color. Inner if statement fixes bug that fills in entire grid
+    // While mouse pointer is pressed and within grid boundaries, fills cell with selected color. Inner if statement fixes bug that fills in entire grid
   	if (down) {
       // 'TD' capitalized because element.tagName returns upper case for DOM trees that represent HTML elements
       if (e.target.tagName === 'TD') {
@@ -61,7 +61,14 @@ pixelCanvas.addEventListener('mousedown', function(e) {
 
 makeGrid(25, 25);
 
-// removes color from cell upon double-click
+// Removes color from cell upon double-click
 pixelCanvas.addEventListener('dblclick', e => {
   e.target.style.backgroundColor = null;
+});
+
+// Adds color-fill functionality. e.preventDefault(); intercepts page refresh on button click
+quickFill.addEventListener('click', function(e) {
+  e.preventDefault();
+  const color = document.querySelector('.color-picker').value;
+  pixelCanvas.querySelectorAll('td').forEach(td => td.style.backgroundColor = color);
 });
