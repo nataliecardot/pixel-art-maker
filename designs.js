268a1c0619e3bb519a72fc253eaa19b7a4d16b56
Natalie Cardot <33387780+nataliecardot@users.noreply.github.com>
2018-03-25 21:57:46 -0700

    Add erase and draw modes

--- designs.js
+++ designs.js
@@ -1,6 +1,8 @@
 const sizePicker = document.querySelector('.size-picker');
 const pixelCanvas = document.querySelector('.pixel-canvas');
 const quickFill = document.querySelector('.quick-fill');
+const eraseMode = document.querySelector('.erase-mode');
+const drawMode = document.querySelector('.draw-mode');
 
 function makeGrid() {
   let gridHeight = document.querySelector('.input-height').value;
@@ -18,7 +20,6 @@ function makeGrid() {
       gridRow.appendChild(gridCell);
       // Fills in cell with selected color upon mouse press ('mousedown', unlike 'click', doesn't also require release of mouse button)
       gridCell.addEventListener('mousedown', function() {
-        // 'color' defined here rather than globally so JS checks whether user has changed color with each new mouse press on cell
         const color = document.querySelector('.color-picker').value;
         this.style.backgroundColor = color;
       })
@@ -26,14 +27,16 @@ function makeGrid() {
   }
 }
 
+makeGrid(25, 25);
+
 // Upon user's submitting height and width selections, callback function (inside method) calls makeGrid function. But event method preventDefault() first intercepts the 'submit' event, which would normally submit the form and refresh the page, preventing makeGrid() from being processed
 sizePicker.addEventListener('submit', function(e) {
   e.preventDefault();
   makeGrid();
 });
 
-// Enables color dragging with selected color (code for filling in single cell is above)
-let down = false; // tracks whether or not mouse pointer is pressed
+// Enables color dragging with selected color (code for filling in single cell is above). (No click on 'draw' mode needed; this is default mode)
+let down = false; // Tracks whether or not mouse pointer is pressed
 
 // Listens for mouse pointer press and release on grid. Changes value to true when pressed, but sets it back to false as soon as released
 pixelCanvas.addEventListener('mousedown', function(e) {
@@ -59,16 +62,71 @@ pixelCanvas.addEventListener('mousedown', function(e) {
   });
 });
 
-makeGrid(25, 25);
+// Adds color-fill functionality. e.preventDefault(); intercepts page refresh on button click
+quickFill.addEventListener('click', function(e) {
+  e.preventDefault();
+  const color = document.querySelector('.color-picker').value;
+  pixelCanvas.querySelectorAll('td').forEach(td => td.style.backgroundColor = color);
+});
 
 // Removes color from cell upon double-click
 pixelCanvas.addEventListener('dblclick', e => {
   e.target.style.backgroundColor = null;
 });
 
-// Adds color-fill functionality. e.preventDefault(); intercepts page refresh on button click
-quickFill.addEventListener('click', function(e) {
-  e.preventDefault();
-  const color = document.querySelector('.color-picker').value;
-  pixelCanvas.querySelectorAll('td').forEach(td => td.style.backgroundColor = color);
+// DRAW AND ERASE MODES:
+
+// Allows for drag and single-cell erasing upon clicking 'erase' button. Code for double-click erase functionality (Without entering erase mode) is above. Also note 'down' was set to false in variable above
+eraseMode.addEventListener('click', function() {
+  // Enables drag erasing while in erase mode
+  pixelCanvas.addEventListener('mousedown', function(e) {
+  	down = true;
+  	pixelCanvas.addEventListener('mouseup', function() {
+  		down = false;
+  	});
+    // Ensures cells won't be erased if grid is left while pointer is held down
+    pixelCanvas.addEventListener('mouseleave', function() {
+      down = false;
+    });
+    pixelCanvas.addEventListener('mouseover', function(e) {
+      // While mouse pointer is pressed and within grid boundaries, empties cell contents. Inner if statement fixes bug that fills in entire grid
+    	if (down) {
+        if (e.target.tagName === 'TD') {
+        	e.target.style.backgroundColor = null;
+        }
+      }
+    });
+  });
+  // Enables single-cell erase while in erase mode
+  pixelCanvas.addEventListener('click', function(e) {
+    e.target.style.backgroundColor = null;
+  });
+});
+
+// Allows user to return to (default) draw mode after using 'erase' button. Note 'down' was set to false in variable above
+drawMode.addEventListener('click', function() {
+  pixelCanvas.addEventListener('mousedown', function(e) {
+  	down = true;
+  	pixelCanvas.addEventListener('mouseup', function() {
+  		down = false;
+  	});
+    // Ensures cells won't be colored if grid is left while pointer is held down
+    pixelCanvas.addEventListener('mouseleave', function() {
+      down = false;
+    });
+    pixelCanvas.addEventListener('mouseover', function(e) {
+      const color = document.querySelector('.color-picker').value;
+      // While mouse pointer is pressed and within grid boundaries, empties cell contents. Inner if statement fixes bug that fills in entire grid
+    	if (down) {
+        if (e.target.tagName === 'TD') {
+        	e.target.style.backgroundColor = color;
+        }
+      }
+    });
+  });
+  // Enables single-cell coloring while in draw mode
+  pixelCanvas.addEventListener('click', function(e) {
+    const color = document.querySelector('.color-picker').value;
+    e.target.style.backgroundColor = color;
+  });
 });
